e52232d2e5bda820cc5dafe8ecdc5d0cf4b7be26
KimHyeonBean <126946754+KimHyeonBean@users.noreply.github.com>
2023-03-15 14:51:05 +0900

    Update rightbar.js

--- rightbar.js
+++ rightbar.js
@@ -1,41 +1,71 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import Addbox from './Addbox';
+import Member from './Member';
 
-const Navbar = () => {
+const Rightbar = () => {
   return (
-    <nav className="navbar">
-      <div className="navElement">
-        <img
-          src="images/sangwon/instagram.png"
-          className="images"
-          alt="insta"
-        />
-        <span id="instaId" className="mainTitle">
-          westagram
-        </span>
-      </div>
-      <div className="navElement">
-        <FontAwesomeIcon className="search" icon={faMagnifyingGlass} />
-        <input className="inputbox" type="text" placeholder="검색" />
-      </div>
-      <div className="navElement">
-        <img
-          src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/bearu/explore.png"
-          className="images"
-          alt="compass"
-        />
-        <img
-          src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/bearu/heart.png"
-          className="images"
-          alt="heart"
-        />
-        <img
-          src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/bearu/profile.png"
-          className="images"
-          alt="myinfo"
+    <div className="mainRight">
+      <div className="feedBox">
+        <Member
+          id="hasang0.0"
+          class="myProfile"
+          name="하상원"
+          image="images/sangwon/mypic.jpeg"
         />
       </div>
-    </nav>
+      <div className="boxes">
+        <div className="addLine">
+          <span className="text type1">스토리</span>
+          <a className="seeAll">
+            <span className="text">모두보기</span>
+          </a>
+        </div>
+        <Member
+          id="id1"
+          class="profile"
+          name="사람1"
+          image="images/sangwon/pic1.jpg"
+        />
+        <Member
+          id="id2"
+          class="profile"
+          name="사람2"
+          image="images/sangwon/pic1.jpg"
+        />
+        <Member
+          id="id3"
+          class="profile"
+          name="사람3"
+          image="images/sangwon/pic1.jpg"
+        />
+      </div>
+
+      <div className="boxes">
+        <div className="addLine">
+          <span className="text type1">회원님을 위한 추천</span>
+          <a className="seeAll">
+            <span className="text">모두보기</span>
+          </a>
+        </div>
+
+        <Member
+          id="id4"
+          name="사람4"
+          class="profile"
+          image="images/sangwon/pic1.jpg"
+          followBtn="팔로우"
+        />
+        <Member
+          id="id5"
+          name="사람5"
+          class="profile"
+          image="images/sangwon/pic1.jpg"
+          followBtn="팔로우"
+        />
+      </div>
+      <Addbox />
+    </div>
   );
 };
+
+export default Rightbar;
